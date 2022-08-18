9d2c2d219f7ec81aff8a330c6fe2cd70bb6600c2
SAVELDEVV <100238207+saveldevv@users.noreply.github.com>
2022-08-18 22:39:50 +0100

    Update Silentaim.lua

--- Silentaim.lua
+++ Silentaim.lua
@@ -1,7 +1,6 @@
-getgenv().SelectedPart = "Head"
-getgenv().VisibiltyCheck = false
-getgenv().TargetESP = true
-getgenv().FOV = 250
-getgenv().CircleVisibility = true
-getgenv().Distance = 500
-loadstring(game:HttpGet("https://raw.githubusercontent.com/yesok3877/Celestial-Silent-Aim/master/Auto-Input", true))()
+
+
+
+
+
+
